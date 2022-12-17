13bf492f2512554d9f637f4a6fab520a37e2848f
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-12-17 09:12:18 +0500

    Navbar

---
+++ main.js
@@ -0,0 +1,11 @@
+var links = document.getElementsByClassName('link')
+for(var i = 0; i <= links.length; i++)
+   addClass(i)
+
+
+function addClass(id){
+   setTimeout(function(){
+      if(id > 0) links[id-1].classList.remove('hover')
+      links[id].classList.add('hover')
+   }, id*750) 
+}
