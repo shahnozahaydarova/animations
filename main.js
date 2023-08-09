12aeb59403937af35911a8c8b31609c6881e2578
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2023-08-09 18:46:13 +0500

    Animatsiyalar

---
+++ main.js
@@ -0,0 +1,16 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+  gsap.to(text, {
+    backgroundSize: '100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: text,
+      start: 'center 80%',
+      end: 'center 20%',
+      scrub: true,
+    },
+  });
+});
